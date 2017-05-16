9985ea6a5bb8d1793a13cf005f5960b5adbe169a
William Caban <william.caban@wwt.com>
2017-05-16 18:36:36 -0500

    Modifying demo to include hostname

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! from " + os.uname()[1]
 
 if __name__ == "__main__":
     application.run()
